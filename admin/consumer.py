cbc1e095b2245155bccab08fc7f8cbb3e88642cc
Abduvaliyev2003 <abduvaliyevasad23@gmail.com>
2026-05-18 22:44:16 +0500

    new

---
+++ admin/consumer.py
@@ -0,0 +1,20 @@
+import pika
+
+params = pika.ConnectionParameters('amqps://nhzsened:sghwS0lh1e9inlPPL40Zs2AU53RAVzCJ@capybara.lmq.cloudamqp.com/nhzsened   ')
+
+connection = pika.BlockingConnection(params)
+
+channel = connection.channel()
+
+channel.queue_declare(queue='admin', durable=True)
+
+def callback(ch, method, properties, body):
+    pass
+
+channel.basic_consume(queue='admin', on_message_callback=callback, auto_ack=True)
+
+print('start')
+
+channel.start_consuming()
+
+channel.close()
--- admin/products/producer.py
+++ admin/products/producer.py
@@ -6,15 +6,6 @@ connection = pika.BlockingConnection(params)
 
 channel = connection.channel()
 
-channel.queue_declare(queue='admin', durable=True)
+def publish():
+    channel.basic_publish(exchange='nhzsened',routing_key='main', body='Hello World')
 
-def callback(ch, method, properties, body):
-    pass
-
-channel.basic_consume(queue='admin', on_message_callback=callback, auto_ack=True)
-
-print('start')
-
-channel.start_consuming()
-
-channel.close()
